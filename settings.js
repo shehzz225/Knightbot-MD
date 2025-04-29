b2769194a25f0c0e79c91c797419ef7b19cf7f50
shehzz225 <shehannayanasara09@gmail.com>
2025-04-29 09:22:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎Bot',
   botName: "Knight Bot",
   botOwner: 'Knight Bot', // Your name
-  ownerNumber: '784472411', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '+94784472411', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
